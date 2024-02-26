5dfbe8666d135378fa417015502abd8294789bb1
sant0s12 <diego.delossantos@mailbox.org>
2024-02-26 22:00:41 +0100

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { base } from '$app/paths';
+	import { page } from '$app/stores';
+	import { Button, Heading } from 'flowbite-svelte';
+</script>
+
+<div class="w-full h-full flex flex-col items-center p-20 space-y-10">
+	<Heading tag="h1" class="w-auto">{$page.status} {$page.error?.message}</Heading>
+	<Heading tag="h1" class="w-auto">Oof</Heading>
+	<Button href="{base}/">Take me home</Button>
+</div>
