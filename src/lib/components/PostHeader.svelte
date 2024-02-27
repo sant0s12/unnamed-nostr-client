287c7d6dde1e8179e30a03222836e03522625f01
sant0s12 <diego.delossantos@mailbox.org>
2024-02-28 00:07:14 +0100

    Sanitize community links

--- src/lib/components/PostHeader.svelte
+++ src/lib/components/PostHeader.svelte
@@ -2,6 +2,7 @@
 	import { base } from '$app/paths';
 	import Avatar from '$lib/components/Avatar.svelte';
 	import { getUserMetadata, npubEncodeShort, type Community, type Post } from '$lib/nostr';
+	import DOMPurify from 'dompurify';
 	import { ArrowsRepeatOutline, BadgeCheckSolid } from 'flowbite-svelte-icons';
 	import { onMount } from 'svelte';
 
@@ -10,15 +11,21 @@
 
 	let communities: Community[] = [];
 	let communityLinks: string[] = [];
+	let joinedLinks: string = '';
 
 	$: {
 		for (const community of communities) {
+			community.name = DOMPurify.sanitize(community.name, {ALLOWED_TAGS: []});
+			community.id = DOMPurify.sanitize(community.id, {ALLOWED_TAGS: []});
+
 			communityLinks.push(
-				`<a href="${base}/c/${community.id}" class="hover:underline" > <p>${community.name}</p> </a>`
+				`<a href="${base}/c/${community.id}" class="hover:underline"> <p>${community.name}</p> </a>`
 			);
+
 			communityLinks = communityLinks;
 		}
-		console.log(communityLinks);
+
+		joinedLinks = communityLinks.join('<span class="text-gray-400"> • </span>');
 	}
 
 	// Get the user metadata
@@ -64,7 +71,7 @@
 	<div class="flex flex-col text-xs justify-center items-start">
 		{#if showCommunity && post.communities && post.communities.length > 0}
 			<div class="flex flex-row space-x-1 font-bold">
-				{@html communityLinks.join('<span class="text-gray-400"> • </span>')}
+				{@html joinedLinks}
 			</div>
 		{/if}
 		<a
