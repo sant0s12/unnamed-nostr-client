bf6159c75f9cb5b964597c959ca85b689db89142
sant0s12 <diego.delossantos@mailbox.org>
2024-03-05 22:28:32 +0100

    Start sidebar layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,11 @@
 		NavLi,
 		NavUl,
 		Navbar,
-		Select
+		Select,
+		Sidebar,
+		SidebarGroup,
+		SidebarItem,
+		SidebarWrapper
 	} from 'flowbite-svelte';
 	import '../app.css';
 	import { base } from '$app/paths';
@@ -20,64 +24,84 @@
 	import theme from '$lib/stores/theme';
 	import { signInOut } from '$lib/auth';
 	import { npubShort } from '$lib/nostr';
+	import { page } from '$app/stores';
 
 	import favicon from '$lib/assets/favicon.png';
+	import { HomeSolid, UsersGroupSolid } from 'flowbite-svelte-icons';
+	import { fly } from 'svelte/transition';
+
+	let dropdownOpen = false;
 </script>
 
-<header class="sticky top-0 z-20">
-	<Navbar fluid={true} class="bg-gray-200 dark:bg-gray-800 py-2">
-		<NavBrand href="{base}/">
-			<img src={favicon} class="me-3 h-6 sm:h-9" alt="Logo" />
-			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-				>My soul left</span
-			>
-		</NavBrand>
-		<div class="flex items-center md:order-2">
-			<Button id="avatar_button" pill class="p-0 md:p-1 my-0 space-x-1" color="light" size="xs">
+<Toaster position="bottom-right" />
+
+<div class="min-h-svh">
+	<header class="sticky top-0 w-full z-20">
+		<Navbar fluid={true} class="bg-gray-200 dark:bg-gray-800 py-0 flex justify-between h-16">
+			<NavBrand href="{base}/">
+				<img src={favicon} class="me-3 h-6 sm:h-9" alt="Logo" />
+				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+					>My soul left</span
+				>
+			</NavBrand>
+			<Button id="avatar-button" pill class="p-0 md:p-1 my-0 space-x-1" color="light" size="xs">
 				<Avatar size="md" src={$ndk.activeUser?.profile?.image} />
 				{#if $ndk.activeUser?.profile?.name}
 					<p class="hidden md:block px-1">{$ndk.activeUser?.profile?.name}</p>
 				{/if}
 			</Button>
-			<NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
-		</div>
-		<NavUl>
-			<NavLi href="{base}/">Home</NavLi>
-			<NavLi href="{base}/communities">Communities</NavLi>
-		</NavUl>
-		<Dropdown triggeredBy="#avatar_button" class="w-56">
-			{#if $ndk.activeUser}
-				<DropdownHeader>
-					<span class="block text-sm">{$ndk.activeUser?.profile?.name ??
-						npubShort($ndk.activeUser.npub)}</span>
-					{#if $ndk.activeUser?.profile?.nip05}
-						<span class="block truncate text-sm font-medium">
-							{$ndk.activeUser?.profile?.nip05}
-						</span>
-					{/if}
-				</DropdownHeader>
-			{/if}
-			<DropdownItem>Settings</DropdownItem>
-			<DropdownDivider />
-			<DropdownItem class="py-1 h-min">
-				<div class="flex flex-row items-center">
-					<p>Theme</p>
-					<Select class="ml-auto w-24 h-min py-1" bind:value={$theme} placeholder="">
-						<option value="light">Light</option>
-						<option value="dark">Dark</option>
-						<option value="system">System</option>
-					</Select>
-				</div>
-			</DropdownItem>
-			<DropdownDivider />
-			<DropdownItem on:click={signInOut}>{$ndk.activeUser ? 'Sign out' : 'Sign in'}</DropdownItem>
-		</Dropdown>
-	</Navbar>
-</header>
+			<Dropdown triggeredBy="#avatar-button" class="w-56" bind:open={dropdownOpen}>
+				{#if $ndk.activeUser}
+					<DropdownHeader>
+						<span class="block text-sm"
+							>{$ndk.activeUser?.profile?.name ?? npubShort($ndk.activeUser.npub)}</span
+						>
+						{#if $ndk.activeUser?.profile?.nip05}
+							<span class="block truncate text-sm font-medium">
+								{$ndk.activeUser?.profile?.nip05}
+							</span>
+						{/if}
+					</DropdownHeader>
+				{/if}
+				<DropdownItem>Settings</DropdownItem>
+				<DropdownDivider />
+				<DropdownItem class="py-1 h-min">
+					<div class="flex flex-row items-center">
+						<p>Theme</p>
+						<Select class="ml-auto w-24 h-min py-1" bind:value={$theme} placeholder="">
+							<option value="light">Light</option>
+							<option value="dark">Dark</option>
+							<option value="system">System</option>
+						</Select>
+					</div>
+				</DropdownItem>
+				<DropdownDivider />
+				<DropdownItem on:click={signInOut}>{$ndk.activeUser ? 'Sign out' : 'Sign in'}</DropdownItem>
+			</Dropdown>
+		</Navbar>
+	</header>
 
-<Toaster position="bottom-right" />
-<div class="px-4 py-4 flex flex-col items-center">
-	<main class="lg:w-2/5 md:w-2/3 w-full">
-		<slot />
-	</main>
+	<div class="grid grid-cols-[1fr_auto_1fr]">
+		<div
+			class="max-h-[calc(100vh-theme(space.16))] max-w-full sticky top-16
+				   overflow-y-auto ml-auto"
+		>
+			<Sidebar asideClass="hidden lg:block" activeUrl={$page.url.pathname}>
+				<SidebarWrapper>
+					<SidebarGroup>
+						<SidebarItem href="{base}/" label="Home">
+							<HomeSolid slot="icon" />
+						</SidebarItem>
+						<SidebarItem href="{base}/communities" label="Communities">
+							<UsersGroupSolid slot="icon" />
+						</SidebarItem>
+					</SidebarGroup>
+				</SidebarWrapper>
+			</Sidebar>
+		</div>
+		<main class="max-w-screen-sm p-5">
+			<slot />
+		</main>
+		<div class="mr-auto"></div>
+	</div>
 </div>
